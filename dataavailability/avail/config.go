eb21563362833540cd6cbc613517924c04950a60
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2025-01-03 21:13:32 +0530

    feat: initial version of availda integration

---
+++ dataavailability/avail/config.go
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+package avail
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type Config struct {
+	Seed         string `json:"seed"`
+	WsApiUrl     string `json:"ws_api_url"`
+	HttpApiUrl   string `json:"http_api_url"`
+	BridgeApiUrl string `json:"bridge_api_url"`
+	AppID        int    `json:"app_id"`
+	Timeout      int    `json:"timeout"`
+}
+
+func (c *Config) GetConfig(configFileName string) error {
+	jsonFile, err := os.Open(configFileName)
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(byteValue, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
